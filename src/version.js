cf235a1c9066cbc978b1865c0c833ec9daad0335
Alf Sanzo <alfredo.sanzo@gmail.com>
2015-10-21 17:35:54 -0300

    Versionado 1.0.5

--- src/version.js
+++ src/version.js
@@ -3,14 +3,16 @@
 // Colaboración: Fundación Sadosky, proyecto Program.AR
 
 UNCDuino = {};
-UNCDuino.Version = 'v1.0.4';
+UNCDuino.Version = 'v1.0.5';
 
-UNCDuino.Historia = '######################################\
-Nuevo en versión 1.0.4:\
-   Arreglado bloque "repetir"\
-   Arreglados bloques de melodías musicales\
-   Añadida animación a la pantalla de "compilando"\
-   Añadido un mejor reporte de errores.\
-   Ahora se distribuye con hidUploader v1.2, que corrige errores de carga en Win64.\
- ######################################\
- '
+UNCDuino.Historia = 
+'\
+	Nuevo en versión 1.0.5:\
+	   Añadido un mejor reporte de errores.\
+	   Ahora se distribuye con hidUploader v1.2, que corrige errores de carga en Win64.\
+	   Ahora se muestra la versión actual en el título\
+	Nuevo en versión 1.0.4:\
+	   Arreglado bloque "repetir"\
+	   Arreglados bloques de melodías musicales\
+	   Añadida animación a la pantalla de "compilando"\
+'

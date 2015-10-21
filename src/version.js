3b0ad958b392e9b29c178172c8b28e5062469c81
Alf Sanzo <alfredo.sanzo@gmail.com>
2015-10-21 17:28:43 -0300

    agregando versión al título. No sé si es la mejor manera de hacerlo

---
+++ src/version.js
@@ -0,0 +1,16 @@
+// UNC++Duino
+// Este es un desarrollo de la Universidad Nacional de Córdoba
+// Colaboración: Fundación Sadosky, proyecto Program.AR
+
+UNCDuino = {};
+UNCDuino.Version = 'v1.0.4';
+
+UNCDuino.Historia = '######################################\
+Nuevo en versión 1.0.4:\
+   Arreglado bloque "repetir"\
+   Arreglados bloques de melodías musicales\
+   Añadida animación a la pantalla de "compilando"\
+   Añadido un mejor reporte de errores.\
+   Ahora se distribuye con hidUploader v1.2, que corrige errores de carga en Win64.\
+ ######################################\
+ '
